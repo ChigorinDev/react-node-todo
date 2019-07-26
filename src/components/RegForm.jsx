fdf25841090719f1cf12fc4e6c50005dabd9b1dd
Nastasia Serheencko <nastasia.serheencko@yandex.ru>
2019-07-26 16:45:13 +0300

    add login form

--- src/components/RegForm.jsx
+++ src/components/RegForm.jsx
@@ -18,23 +18,45 @@ class RegForm extends Component {
 
   sendRegister = async data => {
     try {
-      const resp = await Axios.post('http://localhost:4040/api/register', data);
-      console.log('response from API POST:' + resp);
+      const res = await Axios.post('http://localhost:4040/api/register', data);
+      console.log('response from API POST:' + JSON.stringify(res));
     } catch (error) {
       console.log(error.response.data);
     }
+  };
+
+  sendLogin = async data => {
+    try {
+      const token = await Axios.post('http://localhost:4040/api/login', data);
+      console.log('response from Login :' + JSON.stringify(token).data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  handleSubmitLogin = e => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    this.sendLogin({ email, password });
+    console.log({ email, password });
   };
 
   handleSubmit = e => {
     e.preventDefault();
     console.log('Submited', this.state);
     this.sendRegister(this.state);
+    this.setState({
+      userName: '',
+      email: '',
+      password: ''
+    });
   };
   render() {
     return (
       <div className="container">
         <div className="row">
           <form className="col s12">
+            <h2>Register</h2>
             <div className="row">
               <div className="input-field col s12">
                 <input
@@ -75,6 +97,48 @@ class RegForm extends Component {
             </div>
           </form>
         </div>
+        <div className="row">
+          <form className="col s12">
+            <h2>Login</h2>
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  name="password"
+                  onChange={this.handleChange}
+                  type="text"
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <input name="email" onChange={this.handleChange} type="text" />
+                <label htmlFor="userEmail">Email</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col s12">
+                <button
+                  onClick={this.handleSubmitLogin}
+                  className="btn waves-effect waves-light"
+                  type="submit"
+                  name="action"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <button
+          onClick={this.getPosts}
+          className="btn waves-effect waves-light"
+          type="submit"
+          name="action"
+        >
+          get posts
+        </button>
       </div>
     );
   }
